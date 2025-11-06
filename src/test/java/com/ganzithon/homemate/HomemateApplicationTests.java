f7d309dd92ae64ff38d2f754c8ccc9cd367f0287
Yang-Boyun <boyunyang@naver.com>
2025-11-06 22:28:54 +0900

    init: first commit for GanziThon BE

---
+++ src/test/java/com/ganzithon/homemate/HomemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganzithon.homemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
